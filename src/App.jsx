538ecf016f194414beca583f8d1d49247156306f
lightflash03 <vamgamer03@gmail.com>
2024-12-09 17:42:34 +0530

    add: list page images

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
-import NavBar from "./components/navbar/Navbar";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./routes/homePage/HomePage";
-import "./layout.scss";
+import Layout from "./routes/layout/layout";
+import ListPage from "./routes/listPage/listPage";
+import SinglePage from "./routes/singlePage/singlePage";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [ // Represent the Outlet in Layout.jsx
+        {
+          path: "/",
+          element: <HomePage />
+        },
+        {
+          path: "/list",
+          element: <ListPage />
+        },
+        {
+          path: "/:id", // Dynamic URL
+          element: <SinglePage />
+        }
+      ]
+    }
+  ]);
+
   return (
-    <div className="layout">
-      <div className="navbar">
-        <NavBar />
-      </div>
-      <div className="content">
-        <HomePage />
-      </div>
-    </div>
+    <RouterProvider router={router}/>
   )
 }
 
